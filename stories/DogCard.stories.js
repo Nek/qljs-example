3e94f2cac8a0e2447c5650b2df1ca7d797271a37
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-27 16:04:06 +0200

    Eject react app and fix toolchain errors

--- stories/DogCard.stories.js
+++ stories/DogCard.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
-import DogCard from '../components/DogCard'
+import DogCard from '../src/components/DogCard'
 
 storiesOf('DogCard', module).add('default', () => <DogCard />)
