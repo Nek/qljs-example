f497c5cc8dee89851adf6ab9bf489216f2cbe0f1
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-27 15:38:49 +0200

    Initial app

---
+++ src/ components/DogCard.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const DogCard = () => <div>Dog Breed</div>
+
+export default DogCard
---
+++ src/stories/DogCard.story.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import DogCard from '../components/DogCard'
+
+storiesOf('DogCard', module).add('default', () => <DogCard />)
