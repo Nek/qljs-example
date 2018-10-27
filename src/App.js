417c6b2903e71563e0fe98f8b31fdd90fd8b481c
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-27 16:52:55 +0200

    Fix a wrong path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import DogCard from './ components/DogCard'
+import DogCard from './components/DogCard'
 
 class App extends Component {
   render() {
