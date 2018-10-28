470fc9507d48a5da9ff864333bb0fef2f8f4d3cd
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-28 16:06:09 +0100

    Implement dogs data loading
    Only dogs data itself is loaded. No images and
    descriptions yet.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,54 @@ import './App.css'
 import DogCard from './components/DogCard'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      breeds: [],
+    }
+  }
+  componentDidMount() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(body => body.json())
+      .then(({ message }) => {
+        const data = Object.entries(message)
+          .map(
+            ([breedName, subbreeds]) =>
+              subbreeds.length > 0
+                ? subbreeds.map(subBreedName => [breedName, subBreedName])
+                : [[breedName]],
+          )
+          .reduce((acc, val) => [...acc, ...val])
+          .map(dogBreedPair => {
+            const [breedName, subBreedName] = dogBreedPair
+            const subBreedText = subBreedName ? ` - ${subBreedName}` : ''
+            const fullBreedName = breedName + subBreedText
+            return {
+              breedName,
+              subBreedName,
+              fullBreedName,
+              imageSrcDataUrl:
+                `https://dog.ceo/api/breed/` +
+                dogBreedPair.join('/') +
+                `/images/random`,
+              descriptionTextUrl:
+                'https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1',
+              imageUrl: null,
+              description: '',
+            }
+          })
+        return data
+      })
+      .then(breeds => {
+        this.setState({ breeds })
+        return breeds
+      })
+      .then(breeds => {
+        console.log(breeds)
+      })
+  }
   render() {
-    return <DogCard />
+    return this.state.breeds.map(DogCard)
   }
 }
 
