4e80013c9975620324bc95203b08831ab12ca908
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-28 16:05:26 +0100

    Add basic DogCard implementation

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const DogCard = () => <div>Dog Breed</div>
+const DogCard = ({ fullBreedName, imageSrc, description }) => {
+  return (
+    <div className=".dog-card">
+      <h1>{fullBreedName}</h1>
+      <img src={imageSrc} alt={fullBreedName} />
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default DogCard
--- stories/DogCard.stories.js
+++ stories/DogCard.stories.js
@@ -3,4 +3,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import DogCard from '../src/components/DogCard'
 
-storiesOf('DogCard', module).add('default', () => <DogCard />)
+storiesOf('DogCard', module).add('default', () => (
+  <DogCard imageSrc="/dog.jpg" />
+))
