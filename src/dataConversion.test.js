af97e015f858279a2592df513469d10114257fe9
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-28 20:14:23 +0100

    Add rawDataToAppData test

--- src/dataConversion.test.js
+++ src/dataConversion.test.js
@@ -37,4 +37,28 @@ describe('dataConversion', () => {
       })
     })
   })
+  it('converts raw API data to app data', () => {
+    expect(
+      rawDataToAppData({ message: { hound: [], basset: ['grey', 'red'] } }),
+    ).toEqual([
+      {
+        fullBreedName: 'hound',
+        breedId: 'hound',
+        imageSrc: null,
+        description: null,
+      },
+      {
+        fullBreedName: 'basset - grey',
+        breedId: 'basset/grey',
+        imageSrc: null,
+        description: null,
+      },
+      {
+        fullBreedName: 'basset - red',
+        breedId: 'basset/red',
+        imageSrc: null,
+        description: null,
+      },
+    ])
+  })
 })
