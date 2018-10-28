92a48271adf20d5563a91f0b59c5852d0da31068
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-28 21:59:12 +0100

    Add App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+import Enzyme from 'enzyme'
+import { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+it(`loads the data and renders`, () => {
+  const result = { message: { hound: [], basset: ['grey', 'red'] } }
+  fetch.once(JSON.stringify(result))
+  fetch.once(JSON.stringify({ status: 'success', message: 'some_image' }))
+  fetch.once(JSON.stringify({ status: 'success', message: 'some_image' }))
+  fetch.once(JSON.stringify({ status: 'success', message: 'some_image' }))
+  fetch.once(JSON.stringify(['description']))
+  fetch.once(JSON.stringify(['description']))
+  fetch.once(JSON.stringify(['description']))
+
+  const wrapper = mount(<App />)
+
+  setImmediate(() => {
+    expect(wrapper.state('breeds')).toEqual([
+      {
+        fullBreedName: 'hound',
+        breedId: 'hound',
+        imageSrc: 'some_image',
+        description: 'description',
+      },
+      {
+        fullBreedName: 'basset - grey',
+        breedId: 'basset/grey',
+        imageSrc: 'some_image',
+        description: 'description',
+      },
+      {
+        fullBreedName: 'basset - red',
+        breedId: 'basset/red',
+        imageSrc: 'some_image',
+        description: 'description',
+      },
+    ])
+  })
 })
