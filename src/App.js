ee46921a0ab21be8c5112c007b200fddd5d66bce
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-28 19:33:02 +0100

    Extract state updaters

--- src/App.js
+++ src/App.js
@@ -5,54 +5,10 @@ import DogCard from './components/DogCard'
 import { fetchAllBreeds, fetchImageSrc, fetchDescription } from './api'
 
 import { rawDataToAppData } from './dataConversion'
-
-const findBreedAndIndex = (breeds, fullBreedName) => {
-  const breedIndex = breeds.findIndex(
-    breed => breed.fullBreedName === fullBreedName,
-  )
-  const breed = breeds[breedIndex]
-  return { breedIndex, breed }
-}
-
-const setImageSrc = ({ breeds, fullBreedName, imageSrc }) => {
-  const { breedIndex, breed } = findBreedAndIndex(breeds, fullBreedName)
-
-  const newBreeds = [...breeds]
-  newBreeds[breedIndex] = { ...breed, imageSrc }
-
-  return newBreeds
-}
-
-const setDescription = ({ breeds, fullBreedName, description }) => {
-  const { breedIndex, breed } = findBreedAndIndex(breeds, fullBreedName)
-
-  const newBreeds = [...breeds]
-  newBreeds[breedIndex] = { ...breed, description }
-
-  return newBreeds
-}
-
-const updateStateWithImageSrc = setState => promise =>
-  promise.then(data => {
-    setState(state => {
-      const { breeds } = state
-      const newBreeds = setImageSrc({ ...data, breeds })
-      return {
-        breeds: newBreeds,
-      }
-    })
-  })
-
-const updateStateWithDescription = setState => promise =>
-  promise.then(data => {
-    setState(state => {
-      const { breeds } = state
-      const newBreeds = setDescription({ ...data, breeds })
-      return {
-        breeds: newBreeds,
-      }
-    })
-  })
+import {
+  updateStateWithImageSrc,
+  updateStateWithDescription,
+} from './stateUpdaters'
 
 class App extends Component {
   constructor(props) {
