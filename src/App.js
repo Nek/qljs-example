3bf2b430b5833d72fd83942191607faef3e8d30c
Nikita Dudnik <nikdudnik@gmail.com>
2018-10-28 19:02:51 +0100

    Extract setImageSrc and setDescription

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Component } from 'react'
 import './App.css'
 import DogCard from './components/DogCard'
 
+const flatten = (acc, val) => [...acc, ...val]
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
+
+const extractJson = body => body.json()
+
 const API_ROOT = `https://dog.ceo/api/`
 const BREEDS_LIST_ALL = `${API_ROOT}breeds/list/all`
 const breedIdImagesRandomUrl = breedId =>
   `${API_ROOT}breed/` + breedId + `/images/random`
-
-const flatten = (acc, val) => [...acc, ...val]
 
 const breedDataToPairs = ([breedName, subbreeds]) =>
   subbreeds.length > 0
@@ -36,7 +39,7 @@ const rawDataToAppData = ({ message }) => {
 
 const fetchImageSrc = ({ fullBreedName, breedId }) => {
   return fetch(breedIdImagesRandomUrl(breedId))
-    .then(body => body.json())
+    .then(extractJson)
     .then(({ status, message }) => {
       return {
         imageSrc: status === 'success' && message,
@@ -49,7 +52,7 @@ const fetchDescription = ({ fullBreedName }) => {
   return fetch(
     'https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1',
   )
-    .then(body => body.json())
+    .then(extractJson)
     .then(([description]) => ({ fullBreedName, description }))
 }
 
@@ -61,6 +64,24 @@ const findBreedAndIndex = (breeds, fullBreedName) => {
   return { breedIndex, breed }
 }
 
+const setImageSrc = ({ breeds, fullBreedName, imageSrc }) => {
+  const { breedIndex, breed } = findBreedAndIndex(breeds, fullBreedName)
+
+  const newBreeds = [...breeds]
+  newBreeds[breedIndex] = { ...breed, imageSrc }
+
+  return newBreeds
+}
+
+const setDescription = ({ breeds, fullBreedName, description }) => {
+  const { breedIndex, breed } = findBreedAndIndex(breeds, fullBreedName)
+
+  const newBreeds = [...breeds]
+  newBreeds[breedIndex] = { ...breed, description }
+
+  return newBreeds
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -70,7 +91,7 @@ class App extends Component {
   }
   componentDidMount() {
     fetch(BREEDS_LIST_ALL)
-      .then(body => body.json())
+      .then(extractJson)
       .then(rawDataToAppData)
       .then(breeds => {
         this.setState({ breeds })
@@ -83,17 +104,7 @@ class App extends Component {
               const { breeds } = this.state
               return { ...data, breeds }
             })
-            .then(({ breeds, fullBreedName, imageSrc }) => {
-              const { breedIndex, breed } = findBreedAndIndex(
-                breeds,
-                fullBreedName,
-              )
-
-              const newBreeds = [...breeds]
-              newBreeds[breedIndex] = { ...breed, imageSrc }
-
-              return newBreeds
-            })
+            .then(setImageSrc)
             .then(newBreeds => {
               this.setState({
                 breeds: newBreeds,
@@ -106,17 +117,7 @@ class App extends Component {
               const { breeds } = this.state
               return { ...data, breeds }
             })
-            .then(({ breeds, fullBreedName, description }) => {
-              const { breedIndex, breed } = findBreedAndIndex(
-                breeds,
-                fullBreedName,
-              )
-
-              const newBreeds = [...breeds]
-              newBreeds[breedIndex] = { ...breed, description }
-
-              return newBreeds
-            })
+            .then(setDescription)
             .then(newBreeds => {
               this.setState({
                 breeds: newBreeds,
