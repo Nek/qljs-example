6cf02f18723d4efa879ab4dacb87ff5c65a40087
Nikita Dudnik <nikdudnik@gmail.com>
2019-01-06 15:03:05 +0100

    Group parsers

--- src/parsers/index.js
+++ src/parsers/index.js
@@ -2,9 +2,8 @@ import { parseChildren, parseChildrenRemote, parsers } from 'qljs'
 
 let { read, mutate, remote, sync } = parsers
 
-// query name
-// environment
-// state
+// Todos
+
 read['todoText'] = (term, { todoId }, state) => {
   const text = state.todos[todoId] && state.todos[todoId].text
   return text
@@ -32,6 +31,48 @@ read['todos'] = (term, env, state) => {
   }
 }
 
+mutate['todoNew'] = ([key, { area, text, todoId }], env, state) => {
+  state.todos[todoId] = { text, area }
+  return state.todos
+}
+
+mutate['todoDelete'] = (term, { areaId, todoId }, state) => {
+  const newTodos = { ...state.todos }
+  delete newTodos[todoId]
+  state.todos = newTodos
+  return state
+}
+
+remote['todoNew'] = (queryTerm, state) => {
+  return queryTerm
+}
+
+remote['todoDelete'] = (queryTerm, state) => {
+  return queryTerm
+}
+
+remote['todos'] = (queryTerm, state) => {
+  return parseChildrenRemote(queryTerm)
+}
+
+sync['todoDelete'] = (queryTerm, result, env, state) => {}
+
+sync['todoNew'] = ([tag, { todoId }], { id }, env, state) => {
+  const todo = state.todos[todoId]
+  delete state.todos[todoId]
+  state.todos[id] = todo
+}
+
+// Areas
+
+read['areaTitle'] = (term, { areaId }, state) => {
+  return state.areas[areaId] && state.areas[areaId].title
+}
+
+read['areaId'] = (term, { areaId }, state) => {
+  return state.areas[areaId] && areaId
+}
+
 read['areas'] = (term, env, state) => {
   const [, { areaId }] = term
   if (areaId) {
@@ -45,28 +86,12 @@ read['areas'] = (term, env, state) => {
   }
 }
 
-read['areaTitle'] = (term, { areaId }, state) => {
-  return state.areas[areaId] && state.areas[areaId].title
+remote['areaTitle'] = (queryTerm, state) => {
+  return queryTerm
 }
 
-read['areaId'] = (term, { areaId }, state) => {
-  return state.areas[areaId] && areaId
-}
-
-read['appLoading'] = (term, env, state) => {
-  return state.loading
-}
-
-mutate['appInit'] = (term, env, state) => {
-  state.loading = true
-  return state
-}
-
-mutate['todoDelete'] = (term, { areaId, todoId }, state) => {
-  const newTodos = { ...state.todos }
-  delete newTodos[todoId]
-  state.todos = newTodos
-  return state
+remote['areas'] = (queryTerm, state) => {
+  return parseChildrenRemote(queryTerm)
 }
 
 mutate['areaDelete'] = (term, { areaId }, state) => {
@@ -77,47 +102,25 @@ mutate['areaDelete'] = (term, { areaId }, state) => {
   return state.areas
 }
 
-mutate['todoNew'] = ([key, { area, text, todoId }], env, state) => {
-  state.todos[todoId] = { text, area }
-  return state.todos
+sync['areas'] = (queryTerm, result, env, state) => {}
+
+// App
+
+read['appLoading'] = (term, env, state) => {
+  return state.loading
 }
 
-remote['todoNew'] = (queryTerm, state) => {
-  return queryTerm
-}
-
-remote['todoDelete'] = (queryTerm, state) => {
-  return queryTerm
-}
-
-remote['areaTitle'] = (queryTerm, state) => {
-  return queryTerm
-}
-
-remote['todos'] = (queryTerm, state) => {
-  return parseChildrenRemote(queryTerm)
-}
-
-remote['areas'] = (queryTerm, state) => {
-  return parseChildrenRemote(queryTerm)
+mutate['appInit'] = (term, env, state) => {
+  state.loading = true
+  return state
 }
 
 remote['appInit'] = (queryTerm, state) => {
   return queryTerm
 }
 
-sync['areas'] = (queryTerm, result, env, state) => {}
-
-sync['todoDelete'] = (queryTerm, result, env, state) => {}
-
 sync['appInit'] = (term, result, env, state) => {
   delete state.loading
   state.todos = result.todos
   state.areas = result.areas
-}
-
-sync['todoNew'] = ([tag, { todoId }], { id }, env, state) => {
-  const todo = state.todos[todoId]
-  delete state.todos[todoId]
-  state.todos[id] = todo
 }
