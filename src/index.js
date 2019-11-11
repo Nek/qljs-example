5604f8c77be1f78d923f2bad7fd957f622f75fc5
Nikita Nek Dudnik <nikdudnik@gmail.com>
2019-11-11 17:01:40 +0100

    Back to basics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './parsers'
-import { init, component } from 'qljs'
+import { init, component, getQuery } from 'qljs'
 import uuid from 'uuid'
 import './App.css'
 
@@ -24,7 +24,7 @@ const Todo = component([['todoId', {}], ['text', {}]], props => {
 Todo.displayName = 'Todo'
 
 const Area = component(
-  [['areaId', {}], ['areaTitle', {}], ['todos', {}, Todo]],
+  [['areaId', {}], ['areaTitle', {}], ['todos', {}, getQuery(Todo)]],
   props => {
     const { areaTitle, todos, render } = props
     return (
@@ -46,7 +46,7 @@ const AreaOption = component([['areaId', {}], ['areaTitle', {}]], props => {
 AreaOption.displayName = 'AreaOption'
 
 const TodoList = component(
-  [['areas', {}, Area, AreaOption], ['loading', {}]],
+  [['areas', {}, getQuery(Area), getQuery(AreaOption)], ['loading', {}]],
   props => {
     const { areas, loading, transact, render } = props
 
